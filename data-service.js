bc152233318066de1c803b21b47635bfa8bca777
sdigpal <sdigpal@myseneca.ca>
2022-11-28 13:41:35 -0500

    initial commit

--- data-service.js
+++ data-service.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-var sequelize = new Sequelize('databse', 'user', 'password', {
-    host: 'host',
+var sequelize = new Sequelize('berxvwgt', 'berxvwgt', '31i6hIv2JGXtNytTiW83bV_prG7PERs1', {
+    host: 'peanut.db.elephantsql.com',
     dialect: 'postgres',
     port: 5432,
     dialectOptions: {
