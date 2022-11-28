eea9545475f63a04f9fb32b263f68c373c2d67df
sdigpal <sdigpal@myseneca.ca>
2022-11-28 14:13:27 -0500

    initial commit

--- data-service.js
+++ data-service.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 var sequelize = new Sequelize('berxvwgt', 'berxvwgt', '31i6hIv2JGXtNytTiW83bV_prG7PERs1', {
-    host: 'host',
+    host: 'https://rich-erin-outerwear.cyclic.app',
     dialect: 'postgres',
     port: 5432,
     dialectOptions: {
